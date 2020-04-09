7f43aba0eae97760a7bb4aeb4f9e19fcaf30b5ae
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-04-09 16:09:43 -0300

    Fix format length bug

--- snippets/media_converter.py
+++ snippets/media_converter.py
@@ -6,7 +6,8 @@ cmd2 = ' -vf scale=1280:-1 -c:v libx264 -preset veryfast -crf 23 '
 cmd3 = '.mp4'
 
 
-localFiles = os.popen ('ls *webm').read ().split ('\n')[:-1]
+videoFormat = 'webm'
+localFiles = os.popen ('ls *' + videoFormat).read ().split ('\n')[:-1]
 for f in localFiles:
-  os.system (cmd1 + f + cmd2 + f [:-4] + cmd3)
-  #print (cmd1 + f + cmd2 + f [:-4] + cmd3)
+  os.system (cmd1 + f + cmd2 + f [:- len (videoFormat)] + cmd3)
+  print (cmd1 + f + cmd2 + f [:- len (videoFormat)] + cmd3)
