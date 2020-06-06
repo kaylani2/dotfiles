7fdd156f9c278745c6e2f0c3455026b7c691f823
Kaylani Bochie <kaylani2@poli.ufrj.br>
2020-06-05 22:38:15 -0300

    Add alias

--- arrange_dotfiles.sh
+++ arrange_dotfiles.sh
@@ -4,3 +4,7 @@ touch ~/.ssh/config
 echo "Host *\n\tServerAliveInterval" | sudo tee ~/.ssh/config
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
+touch ~/.bashrc
+echo "alias l='ls -lh'" | sudo tee -a ~/.bashrc
+echo "alias tt='tmux'" | sudo tee -a ~/.bashrc
+echo "alias xx='xdg-open'" | sudo tee -a ~/.bashrc
