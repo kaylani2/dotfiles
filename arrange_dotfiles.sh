5dc14edffadb1c780a865b214f4f9b4a39cee9d3
Kaylani Bochie <kaylani@gta.ufrj.br>
2020-07-25 02:48:09 -0300

    Update .tmux.conf to tmux 2.9

--- arrange_dotfiles.sh
+++ arrange_dotfiles.sh
@@ -12,11 +12,13 @@ cp .tmux.conf ~/.tmux.conf
 
 ## Echoes
 echo -e "Host *\n\tServerAliveInterval 60" | tee ~/.ssh/config
+
 echo "alias l='ls -lh'" | tee -a ~/.bashrc
 echo "alias tt='tmux'" | tee -a ~/.bashrc
 echo "alias xx='xdg-open'" | tee -a ~/.bashrc
 echo "alias teelog='tee logfile.$(date +'%Y-%m-%d-%H_%M_%S').log'"
 echo "stty ixany" | tee -a ~/.bashrc
+
 echo 'if [ -n "$BASH_VERSION" ]; then' | tee -a ~/.profile
 echo '# include .bashrc if it exists' | tee -a ~/.profile
 echo 'if [ -f "$HOME/.bashrc" ]; then' | tee -a ~/.profile
