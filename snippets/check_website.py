4d9999efbaba6721303e12812f301c163d15a646
Kaylani <kaylani@gta.ufrj.br>
2021-07-02 10:12:28 -0300

    Add bs4 to check site

--- snippets/check_website.py
+++ snippets/check_website.py
@@ -1,14 +1,18 @@
 import hashlib
 from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
 URL = 'https://www.gta.ufrj.br/~kaylani/'
 HASH_FILE = 'current_hash.sha'
 
 url = Request (URL, headers = {'User-Agent': 'Mozilla/5.0'})
 
 response = urlopen (url).read ()
-new_hash = hashlib.sha224 (response).hexdigest ()
+soup = BeautifulSoup (response, 'html.parser')
+publications = soup.find ('section', {'id': 'publications'}).text
 
+new_hash = hashlib.sha224 (publications.encode ()).hexdigest ()
 
+### K: Running for the first time.
 try:
   with open (HASH_FILE, 'r') as f:
     old_hash = f.read ()
@@ -22,5 +26,6 @@ with open (HASH_FILE, 'r') as f:
 if (new_hash == old_hash):
   print ('nada mudou')
 else: # enviar email
+  print ('algo mudou')
   with open (HASH_FILE, 'w') as f:
     f.write (new_hash)
